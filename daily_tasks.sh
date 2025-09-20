a6ba3cd6a5354efc110ce856574f4badcd90d48b
viruslox <viruslox@users.noreply.github.com>
2025-09-20 13:40:13 +0200

    Update daily_tasks.sh

--- daily_tasks.sh
+++ daily_tasks.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+PROFILE_FILE=$(find /home -name '.frameflow_profile' 2>/dev/null)
+if [ -f "$PROFILE_FILE" ]; then
+  source "$PROFILE_FILE"
+fi
+if [ -z "$VLXlogs_DIR" ]; then
+  $VLXlogs_DIR="/opt/VLXflowlogs"
+fi
+
+## Clean suite logs
+find $VLXlogs_DIR -type f -mtime +15 -exec rm -v {} \;
+
+## clean system logs
+find /var/log -type f -mtime +30 -exec rm -v {} \;
+
 ## save list of installed packages
 dpkg --get-selections | awk '{print $1}' | grep -vE '^(linux-image|linux-headers|linux-firmware|firmware|grub|nvidia|virtualbox|base-files|desktop|-desktop)' >/home/pkg.list
-
-## clean logs
-find /var/log -type f -mtime +30 -exec rm -v {} \;
-find /opt/frameflow_logs -type f -mtime +15 -exec rm -v {} \;
 
 exit 0
