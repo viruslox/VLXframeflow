ddeaa6ba856678881ee8086a90464e9af5f9a0ab
viruslox <viruslox@users.noreply.github.com>
2025-09-24 23:15:44 +0200

    Update 02_network_configuration.sh

--- 02_network_configuration.sh
+++ 02_network_configuration.sh
@@ -5,107 +5,169 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+if ! sysctl net.mptcp.enabled; then
+	echo "The running kernel does not support multi point TCP"
+fi
+if ! sysctl net.mptcp.enabled &> /dev/null; then
+    echo "[ERR] The running kernel does not support multi point TCP."
+    exit 1
+else
+    if [ "$(sysctl -n net.mptcp.enabled)" -eq 1 ]; then
+        echo "[OK] MPTCP enabled."
+    else
+        echo "[WARN] MPTCP currently not enabled."
+        sysctl -w net.mptcp.enabled=1
+		echo "[OK] MPTCP enabled."
+    fi
+fi
+
 NORM_PROFILE="/etc/systemd/network/profiles/normal"
 AP_PROFILE="/etc/systemd/network/profiles/ap-bonding"
 mkdir -p $NORM_PROFILE $AP_PROFILE
 
-cat <<EOF > $AP_PROFILE/10-bond0.netdev
-[NetDev]
-Name=bond0
-Kind=bond
+ufw allow 22/tcp
+sed -i '/^DEFAULT_FORWARD_POLICY/c\DEFAULT_FORWARD_POLICY="ACCEPT"' /etc/default/ufw
+cp -p /etc/ufw/before.rules /etc/ufw/before.rules.BK
 
-[Bond]
-Mode=active-backup
-EOF
+if ! grep -qF "VLXframelow NAT table rules" /etc/ufw/before.rules; then
+	cat <<EOF > /etc/ufw/before.rules.add
+	# VLXframelow NAT table rules
+	*nat
+	:POSTROUTING ACCEPT [0:0]
 
-cat <<EOF > $AP_PROFILE/20-bond0.network
-[Match]
-Name=bond0
+	# MODIFICA: Regola MASQUERADE generale per tutte le interfacce in uscita
+	-A POSTROUTING -s 192.168.168.0/24 -j MASQUERADE
 
-[Network]
-DHCP=yes
-EOF
+	COMMIT
+	# End of VLXframeflow NAT rules
+	EOF
+
+	cat /etc/ufw/before.rules.add /etc/ufw/before.rules.BK > /etc/ufw/before.rules
+fi
 
 ## for each interface create profiles
-for iface in $(ls /sys/class/net); do
-    if [ "$iface" == "lo" ]; then
-        continue
-    fi
+INTERFACES=($(iwconfig 2>/dev/null | grep 'IEEE' | awk '{print $1}'))
+if [ ${#INTERFACES[@]} -eq 0 ]; then
+    echo "No wireless network interface found."
+    exit 1
+fi
 
-    if [ -d "/sys/class/net/$iface/wireless" ]; then
-### wireless interfaces
-cat <<EOF > $NORM_PROFILE/20-$iface.network
+for iface in "${INTERFACES[@]}"; do
+    cat <<EOF > "$NORM_PROFILE/20-$iface.network"
 [Match]
 Name=$iface
 
 [Network]
 DHCP=yes
 WPAConfigFile=/etc/wpa_supplicant/wpa_supplicant-$iface.conf
+
+[Address]
+MPTCPSubflow=no
 EOF
 
-cat <<EOF > $AP_PROFILE/40-$iface-ap.network
+    if [[ "$iface" == "${INTERFACES[0]}" ]]; then
+		ufw allow in on $iface from any port 68 to any port 67 proto udp
+		ufw allow in on $iface from any to any port 53
+        cat <<EOF > "$AP_PROFILE/40-$iface-ap.network"
 [Match]
 Name=$iface
+WLANMode=ap
 
 [Network]
-Address=192.168.10.1/24
+Address=192.168.168.1/24
 IPMasquerade=yes
 DHCPServer=yes
+
+[Address]
+MPTCPSubflow=no
 
 [DHCPServer]
 DNS=8.8.8.8 1.1.1.1
 EOF
 
-cat <<EOF > /etc/hostapd/hostapd.conf
+        cat <<EOF > /etc/hostapd/hostapd.conf
 interface=$iface
 driver=nl80211
 hw_mode=g
 channel=7
-
+wmm_enabled=1
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
 ssid=VLXnetflow
 wpa=2
 wpa_passphrase=LaTuaPasswordSuperSegreta
 wpa_key_mgmt=WPA-PSK
+wpa_pairwise=CCMP
 rsn_pairwise=CCMP
 EOF
 
-	rm -f /etc/wpa_supplicant/wpa_supplicant-$iface.conf 2>/dev/null
-	for knownwlans in /etc/NetworkManager/system-connections/* ; do
-		if grep -q 'type=wifi' $knownwlans; then
-			echo 'network={' >> /etc/wpa_supplicant/wpa_supplicant-$iface.conf
-			grep -h -E '^ssid=|^psk=' $knownwlans | awk -F'=' '{print "    " $1"=\""$2"\""}' >> "/etc/wpa_supplicant/wpa_supplicant-$iface.conf"
-			echo '}' >> /etc/wpa_supplicant/wpa_supplicant-$iface.conf
-		fi
-	done
-	
-	systemctl enable wpa_supplicant@$iface.service
+    WPA_CONF="/etc/wpa_supplicant/wpa_supplicant-$iface.conf"
+    cp -p "$WPA_CONF" "${WPA_CONF}_$(date +%Y-%m-%d_%Hh%Mm)" 2>/dev/null
 
-    elif [[ "$iface" != *bond* ]]; then
-## Ethernet and USB net interfaces
-cat <<EOF > $NORM_PROFILE/10-$iface.network
+    for knownwlans in /etc/NetworkManager/system-connections/*; do
+        if grep -q 'type=wifi' "$knownwlans"; then
+            SSID=$(grep -oP 'ssid=\K.*' "$knownwlans")
+            PSK=$(grep -oP 'psk=\K.*' "$knownwlans")
+
+            if [ -n "$SSID" ] && [ -n "$PSK" ]; then
+                echo 'network={' >> "$WPA_CONF"
+                echo "    ssid=\"$SSID\"" >> "$WPA_CONF"
+                echo "    psk=\"$PSK\"" >> "$WPA_CONF"
+                echo '}' >> "$WPA_CONF"
+            fi
+        fi
+    done
+    
+    systemctl enable "wpa_supplicant@$iface.service"
+	else
+	    cat <<EOF > "$AP_PROFILE/20-$iface.network"
 [Match]
 Name=$iface
 
 [Network]
 DHCP=yes
-EOF
+WPAConfigFile=/etc/wpa_supplicant/wpa_supplicant-$iface.conf
 
-cat <<EOF > $AP_PROFILE/30-$iface-bond.network
-[Match]
-Name=$iface
-
-[Network]
-Bond=bond0
+[Address]
+MPTCPSubflow=no
 EOF
-    fi
+	fi
 done
 
+cat <<EOF > /etc/systemd/system/hostapd.service
+[Unit]
+Description=Access point and authentication server for Wi-Fi and Ethernet
+Documentation=man:hostapd(8)
+After=network.target
+ConditionFileNotEmpty=/etc/hostapd/hostapd.conf
+
+[Service]
+#Type=forking
+Type=exec
+#PIDFile=/run/hostapd.pid
+Restart=on-failure
+RestartSec=2
+Environment=DAEMON_CONF=/etc/hostapd/hostapd.conf
+EnvironmentFile=-/etc/default/hostapd
+#ExecStart=/usr/sbin/hostapd -B -P /run/hostapd.pid $DAEMON_OPTS ${DAEMON_CONF}
+ExecStart=/usr/sbin/hostapd -P /run/hostapd.pid $DAEMON_OPTS ${DAEMON_CONF}
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 ## Enable the new settings
+apt-get update
+apt-get -y install mptcpd
+systemctl daemon-reload
 systemctl disable NetworkManager
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
+systemctl enable mptcpd
 
+ufw reload
+ufw enable
 echo "Now You should restart to give a try - ...keep monitor and keyboard on Your arm lenght range :D"
 
 exit 0
