4539ec04d6a6b7d4b741b49160258c0c81877dc2
viruslox <viruslox@users.noreply.github.com>
2025-10-16 15:17:42 +0200

    Refactor profile validation and copy logic

--- VLX_netflow.sh
+++ VLX_netflow.sh
@@ -5,10 +5,9 @@ PROFILE_NAME=$1
 WIFI_IF=$(ls /sys/class/net/ | while read iface; do [ -d "/sys/class/net/$iface/wireless" ] && echo $iface && break; done)
 
 ## Validate input args
-PROFILES_PATH="/etc/systemd/network/profiles"
-if [ ! -d "$PROFILES_PATH/$PROFILE_NAME" ]; then
-    echo "[ERR] Please give a valid profile name"
-    echo "[INFO]: Existing profiles:"
+if [ -z "$PROFILE_NAME" ] || [ ! -d "$PROFILES_PATH/$PROFILE_NAME" ] ; then
+    echo "[ERR]: Please provide a valid network profile name. Usage: $0 <nome_profilo>"
+    echo "[INFO]: Available network profiles:"
     for profile in "$PROFILES_PATH"/*; do
         if [ -d "$profile" ]; then
             echo "  - $(basename "$profile")"
@@ -26,17 +25,16 @@ systemctl disable wpa_supplicant@$WIFI_IF.service
 rm -f /etc/systemd/network/*.network /etc/systemd/network/*.netdev
 
 # Copy profiles settings
-cp "$PROFILES_PATH"/* /etc/systemd/network/
+cp "$PROFILES_PATH/$PROFILE_NAME"/* /etc/systemd/network/
 
 systemctl restart systemd-networkd
+    sleep 2
 
 if [ "$PROFILE_NAME" = "normal" ]; then
-    sleep 2
     ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
     systemctl enable wpa_supplicant@$WIFI_IF.service
     systemctl start wpa_supplicant@$WIFI_IF.service
 elif [ "$PROFILE_NAME" = "ap-bonding" ]; then
-    sleep 2
     ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
     systemctl enable hostapd
     systemctl start hostapd
